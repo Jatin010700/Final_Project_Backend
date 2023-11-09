9e337e0e47e0cd954d884461c7d70af43443efc9
Jatin010700 <130746825+Jatin010700@users.noreply.github.com>
2023-11-09 12:41:28 +0400

    ;}

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const bcrypt = require('bcrypt');
 const cors = require('cors')
 const nodemailer = require('nodemailer')
 const multer = require("multer");
+const cloudinary = require('cloudinary').v2;
+const jwt = require('jsonwebtoken');
+
 const dotenv = require("dotenv")
 dotenv.config();
 
@@ -14,23 +17,31 @@ const app = express();
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+    
+cloudinary.config({ 
+  cloud_name: process.env.CLOUDINARY_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET 
+});
 
 // Initialize Knex with PostgreSQL database configuration
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.db_host,
-    user: process.env.db_user,
-    password: process.env.db_password,
-    database: process.env.db_DB,
+    host: process.env.DB_LOCAL_HOST,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASS,
+    database: process.env.DB_LOCAL_DB,
+    port: process.env.DB_LOCAL_PORT
   }
-
 });
 
 // Middleware to parse JSON body
 app.use(express.json());
-const upload = multer();
-// Register route
+const upload = multer({ dest: 'uploads/' });
+
+//-----------------------------------------------------------------------------------------//
+
 app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -44,13 +55,38 @@ app.use(function (req, res, next) {
   );
   next()
 })
+
+// // Middleware to verify JWT
+// function verifyToken(req, res, next) {
+//   const token = req.headers.authorization;
+
+//   if (!token) {
+//     return res.status(401).json({ error: 'Token not provided' });
+//   }
+
+//   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(403).json({ error: 'Failed to authenticate token' });
+//     }
+
+//     // Store the user ID in the request for further processing
+//     req.userId = decoded.userId;
+//     next();
+//   });
+// }
+// // Example protected route
+// app.get('/secure-route', verifyToken, (req, res) => {
+//   res.json({ message: '👍 You have access to this protected route!' });
+// });
+
+// Register route
 app.post('/register', async (req, res) => {
   const { firstName, lastName, email, username, password } = req.body;
 console.log(firstName)
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const registeredUser = await db('register').insert({
+    await db('register').insert({
       first_name: firstName,
       last_name: lastName,
       email,
@@ -76,7 +112,9 @@ app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const user = await db('register').where({ username }).first();
+    const user = await db('register')
+    .where({ username })
+    .first();
 
     if (!user) {
       res.status(401).json({ error: '✖ USERNAME NOT FOUND' });
@@ -94,6 +132,12 @@ app.post('/login', async (req, res) => {
       username,
       password: user.password
     });
+
+    // // Create a JWT with the user's ID as the payload
+    // const token = jwt.sign({ userId: user.id }, 
+    //   process.env.JWT_SECRET, {
+    //   expiresIn: '30s', // Token expiration time
+    // });
 
     res.json({ message: '👍 LOGIN SUCCESSFUL' });
   } catch (error) {
@@ -172,97 +216,130 @@ app.post('/reset-password', async (req, res) => {
   }
 });
 
-//Find images by Id
-app.get('/api/images/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
+//--------------OWNER_RENTING_THEIR_CAR---------------//
+app.post("/api/owner-data", upload.array('images', 5), async (req, res) => {
+  const { carName, price, rent, username } = req.body;
 
-    const imageData = await db.select('*')
-      .from('images')
-      .where('id', id)
-      .first();
+  try {  
+    // const userID = req.userId;
 
-    if (!imageData) {
-      return res.status(404).json({ error: 'Image not found' });
-    }
-
-    res.setHeader('Content-Type', 'image/jpeg');
-    res.send(imageData.img_data);
-  } catch (err) {
-    console.error('Error occurred fetching image:', err);
-    res.status(500).json({ error: 'Error occurred fetching image' });
-  }
-});
-
-
-//---------------------ADMIN---------------------//
-// upload image to database
-app.post("/upload", upload.single("image"), async (req, res) => {
-  try {
-    const id = req.body.id; // Retrieve the ID from the request body
-    const imageBuffer = req.file.buffer;
-    const imgName = req.file.originalname;
-
-    // Check if the ID already exists in the database
-    const existingImage = await db('images').where('id', id).first();
-    if (existingImage) {
-      return res.status(400).json({ error: "ID already exists" });
-    }
-
-    const insertImg = await db('images').insert({
-      id: id,
-      img_name: imgName,
-      img_data: imageBuffer,
+    const imageURLS = await Promise.all(
+      req.files.map(async (file) => {
+        const result = await cloudinary.uploader.upload(file.path);
+        return result.secure_url; 
+      })
+    );
+ 
+    await db('car_listings').insert({
+      car_name: carName,
+      price: price,
+      rent: rent,
+      image_url: imageURLS,
+      login_id: username
     });
 
-    if (!insertImg) {
-      return res.status(404).json({ error: "Image not uploaded" });
-    }
-
-    res.json({ message: "Image uploaded successfully!" });
-  } catch (err) {
-    console.error("Error occurred inserting image:", err);
-    res.status(500).json({ error: "Error occurred inserting image" });
+ 
+    res.status(200).json({ message: "Data saved!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred saving data' });
   }
 });
 
-// Update route
-app.post("/update/:id", upload.single("image"), async (req, res) => {
-  try {
-    const id = req.params.id;
-    const imageBuffer = req.file.buffer;
-    const imgName = req.file.originalname;
 
-    const updateImg = await db('images')
-      .where('id', id)
-      .update({ img_name: imgName, img_data: imageBuffer });
+//------------------------------------------------------------------------------------------------//
 
-    if (updateImg === 0) {
-      return res.status(404).json({ error: "Database not Updated" });
-    }
-    return res.status(200).json({ message: "Database updated successfully." });
-  } catch (error) {
-    console.error("Error executing database query:", error);
-    return res.status(500).json({ error: "Failed to update the database." });
-  }
-});
+// //Find images by Id
+// app.get('/api/images/:id', async (req, res) => {
+//   try {
+//     const { id } = req.params;
 
-//delete image from database
-app.post('/delete/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
+//     const imageData = await db.select('*')
+//       .from('images')
+//       .where('id', id)
+//       .first();
 
-    const deleteImg = await db('images')
-    .where('id', id)
-    .del();
+//     if (!imageData) {
+//       return res.status(404).json({ error: 'Image not found' });
+//     }
 
-    if (deleteImg === 0) {
-      return res.status(404).json({ error: "Image not Deleted"})
-    }
-    return res.status(200).json({message: "Image Deleted"})
-  } catch (error) {
-    console.error("Server Error", error)
-    return res.status(500).json({ error: "Failed to delete image." });
+//     res.setHeader('Content-Type', 'image/jpeg');
+//     res.send(imageData.img_data);
+//   } catch (err) {
+//     console.error('Error occurred fetching image:', err);
+//     res.status(500).json({ error: 'Error occurred fetching image' });
+//   }
+// });
 
-  }
-})
+
+// //---------------------ADMIN---------------------//
+// // upload image to database
+// app.post("/upload", upload.single("image"), async (req, res) => {
+//   try {
+//     const id = req.body.id; // Retrieve the ID from the request body
+//     const imageBuffer = req.file.buffer;
+//     const imgName = req.file.originalname;
+
+//     // Check if the ID already exists in the database
+//     const existingImage = await db('images').where('id', id).first();
+//     if (existingImage) {
+//       return res.status(400).json({ error: "ID already exists" });
+//     }
+
+//     const insertImg = await db('images').insert({
+//       id: id,
+//       img_name: imgName,
+//       img_data: imageBuffer,
+//     });
+
+//     if (!insertImg) {
+//       return res.status(404).json({ error: "Image not uploaded" });
+//     }
+
+//     res.json({ message: "Image uploaded successfully!" });
+//   } catch (err) {
+//     console.error("Error occurred inserting image:", err);
+//     res.status(500).json({ error: "Error occurred inserting image" });
+//   }
+// });
+
+// // Update route
+// app.post("/update/:id", upload.single("image"), async (req, res) => {
+//   try {
+//     const id = req.params.id;
+//     const imageBuffer = req.file.buffer;
+//     const imgName = req.file.originalname;
+
+//     const updateImg = await db('images')
+//       .where('id', id)
+//       .update({ img_name: imgName, img_data: imageBuffer });
+
+//     if (updateImg === 0) {
+//       return res.status(404).json({ error: "Database not Updated" });
+//     }
+//     return res.status(200).json({ message: "Database updated successfully." });
+//   } catch (error) {
+//     console.error("Error executing database query:", error);
+//     return res.status(500).json({ error: "Failed to update the database." });
+//   }
+// });
+
+// //delete image from database
+// app.post('/delete/:id', async (req, res) => {
+//   try {
+//     const id = req.params.id;
+
+//     const deleteImg = await db('images')
+//     .where('id', id)
+//     .del();
+
+//     if (deleteImg === 0) {
+//       return res.status(404).json({ error: "Image not Deleted"})
+//     }
+//     return res.status(200).json({message: "Image Deleted"})
+//   } catch (error) {
+//     console.error("Server Error", error)
+//     return res.status(500).json({ error: "Failed to delete image." });
+
+//   }
+// })
